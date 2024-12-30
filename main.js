b96e454c126581a877d18920db76fb6a9174c220
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-12-30 15:20:09 -0300

    up

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
     $('header button').click(function() {
         $('form').slideDown();
     })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const enderecoDaNovaTarefa = $('#endereco-nova-tarefa').val();
+        const novoItem = $('<li"></li>');
+
+        $(`<li>${enderecoDaNovaTarefa}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#endereco-nova-tarefa').val('');
+
+        $(novoItem).on('click', function(){
+            $(this).toggleClass('riscado')
+        })
+    })
 })
+
